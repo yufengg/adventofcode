9bd4eef154ab20c62b7cb68fb49a74534d2c2eb5
Yufeng Guo <info@yufengg.com>
2020-12-05 12:20:20 -0500

    day 5

---
+++ day05.py
@@ -0,0 +1,61 @@
+import utils as ut
+
+def parse1(line):
+	return line
+
+def day05p1():
+    print('day 05 part 1')
+    lines = ut.get_file('day05_input.txt', parse1)
+    max_seat_id = 0
+    for line in lines:
+        s = ''
+        for letter in line[:-3]:
+            if letter == 'F': s += '0'
+            if letter == 'B': s += '1'
+        row_num = int(s, 2)
+        s = ''
+        for letter in line[-3:]:
+            if letter == 'L': s += '0'
+            if letter == 'R': s += '1'
+        col_num = int(s, 2)
+        max_seat_id = max(max_seat_id, row_num * 8 + col_num)
+    return max_seat_id
+
+print(day05p1()) # 7:35
+
+def parse2(line):
+	return line
+
+def day05p2():
+    print('day 05 part 2')
+    lines = ut.get_file('day05_input.txt', parse2)
+    max_seat_id = 0
+    seat_id_list = []
+    for line in lines:
+        s = ''
+        for letter in line[:-3]:
+            if letter == 'F': s += '0'
+            if letter == 'B': s += '1'
+        row_num = int(s, 2)
+        s = ''
+        for letter in line[-3:]:
+            if letter == 'L': s += '0'
+            if letter == 'R': s += '1'
+        col_num = int(s, 2)
+        seat_id = row_num * 8 + col_num
+        seat_id_list.append(seat_id)
+        # max_seat_id = max(max_seat_id, row_num * 8 + col_num)
+    seat_id_list.sort()
+
+    seat_id = seat_id_list[0]
+    for val in seat_id_list:
+        if val == seat_id:
+            seat_id += 1
+        else:
+            print(val)
+            seat_id = val+1
+
+
+    return seat_id_list
+
+print(day05p2()) # 12:34 cumulative
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def get_file(filename, parsing_fn):
     return lines
 
 # import utils as ut
-# ut.generate_template(4)
+# ut.generate_template(5)
 def generate_template(day):
     day = format(day, '02d') # left-pad a zero
 
