b50bfe0841d35734f5c0a857e37673610b46336c
Yufeng Guo <info@yufengg.com>
2020-12-14 01:55:55 -0500

    part 2 complete. No fancy modulo used!

--- day13.py
+++ day13.py
@@ -21,7 +21,7 @@ def day13p1():
     print(departs_in, bus_id)
     return  departs_in * bus_id
 
-print(day13p1()) #12min
+# print(day13p1()) #12min
 
 def parse2(line):
 	return line
@@ -67,4 +67,58 @@ def day13p2():
 
     return schedule
 
-print(day13p2())
+# print(day13p2())
+
+def day13p2v2():
+    print('day 13 part 2v2')
+    lines = ut.get_file('day13_input.txt', parse2)
+    # lines = ut.get_file('day13_input_small.txt', parse2)
+
+    offset = 0
+    schedule = []
+    for val in lines[1].split(','):
+        if val.isnumeric():
+            bus_id = int(val)
+            schedule.append((bus_id, offset))
+        offset += 1
+    print(schedule)
+
+    all_buses = [bus_id for bus_id, offset in schedule]
+
+    # find a match with the the first n buses
+    # use that match to update the search_offset (=0)
+    # update the range operators
+    range_top = 1
+    range_step = 1
+    # prev_bus_id = 1
+    search_offset = 0
+    for i, (bus_id, offset) in enumerate(schedule):
+        range_top *= bus_id
+        found = check_match(range_top, range_step, search_offset, schedule[:i+1])
+        if found:
+            search_offset = found
+        print('found', search_offset)
+        range_step *= bus_id
+
+    return  search_offset
+
+def check_match(range_top, range_step, search_offset, schedule):
+    print('every', range_step, 'offset', search_offset, 'sched', schedule)
+    for i in range(0, range_top, range_step):
+        if i == 0:
+            continue
+        t = i + search_offset
+        match = [False] * len(schedule)
+
+        bus_id, offset = schedule[-1]
+        if (t + offset) % bus_id == 0:
+            return t # short version
+        # for j, (bus_id, offset) in enumerate(schedule):
+        #     if (t + offset) % bus_id == 0:
+        #         print('one', t)
+        #         match[j] = True
+        # if all(match):
+        #     return i # return new offset
+    return False
+
+print(day13p2v2()) # 3hr 46min
