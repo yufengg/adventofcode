295cbf52ec6d02fa705b71c4ac905b11096e3bf4
Yufeng Guo <info@yufengg.com>
2020-12-20 03:45:50 -0500

    day 19. Too many possible matches

--- day19.py
+++ day19.py
@@ -37,6 +37,7 @@ class Rule:
         rule = rules_dict[rule_num]
         if isinstance(rule, str) and rule in 'ab':
             self.value = rule
+            self.rule = rule
             return
 
         self.value = []
@@ -52,45 +53,65 @@ class Rule:
                 rule_set.append(Rule(rules_dict, rule_lookup))
             self.value.append(rule_set)
 
-    def eval(self, msg): # take arg, slice it down, recurse
-        if isinstance(self.value, str):
-            if len(msg) == 0:
-                return False, ''
-            # print(msg, self.rule_num)
-            if len(msg) == 1:
-                valid = msg == self.value
-            else:
-                valid = msg[0] == self.value
+    def eval(self, msgs): # take arg, slice it down, recurse
+        responses = [] # (Bool, str)
+        for msg in msgs:
+            if isinstance(self.value, str):
+                if len(msg) == 0:
+                    # responses.append((False, '')) # do nothing if false
+                    continue
+                # print(msg, self.rule_num)
+                if len(msg) >= 1:
+                    valid = msg[0] == self.value
 
-            if valid:
-                msg = msg[1:]
-                print('\t', valid,self.rule_num, msg)  # DEBUG
-            return valid , msg
+                if valid:
+                    msg = msg[1:]
+                    # print('\t', valid,self.rule_num, msg)  # DEBUG
+                    responses.append(msg)
+                continue
 
-        result = []
-        print('checking', self.rule_num, ':', self.rule, msg)  # DEBUG
-        orig_msg = msg + ''
-        for i, rule_set in enumerate(self.value):
-            msg = orig_msg + ''
-            rule_set_valid = []
-            for v in rule_set:
-                valid, msg = v.eval(msg)
-                rule_set_valid.append(valid)
-                if not valid:
+            result = []
+            matched_msgs = []
+            # print('checking', self.rule_num, ':', self.rule, msg)  # DEBUG
+            orig_msg = msg + ''
+            for i, rule_set in enumerate(self.value):
+                rmsg = [orig_msg + '']
+                rule_set_valid = []
+                for v in rule_set:
+                    # print('rule text:', v.rule, type(v))
+                    valid, rmsg = v.eval(rmsg) # result messages[]
+                    rule_set_valid.append(valid)
+                    if not valid:
+                        # DEBUG
+                        # print('ruleset wrong:', self.rule[i], orig_msg, '>', msg)
+                        break
+                if all(rule_set_valid):
                     # DEBUG
-                    print('ruleset wrong:', self.rule[i], orig_msg, '>', msg)
-                    break
-            if all(rule_set_valid):
-                # DEBUG
-                print('ruleset correct:', self.rule[i], orig_msg,'>',msg)  # DEBUG
-                return True, msg
-        #     result.append(all(rule_set_valid))
-        # return any(result), msg
-        return False, orig_msg
+                    # print('ruleset correct:', self.rule[i], orig_msg,'>',msg)  # DEBUG
+                    matched_msgs +=  rmsg
+                    # return True, msg
+
+                result.append(all(rule_set_valid))
+
+            for m in matched_msgs: # flatten multiples
+                responses.append(m)
+
+            # if any(result):
+            #     # if len(matched_msgs) > 1:
+            #     #     print('extra MATCHES!')
+            #     # print('matched msgs', matched_msgs) # HOW MANY?
+            #     return True, responses
+            # else:
+            #     return False, orig_msg
+        if len(responses):
+            return True, responses
+        else:
+            return False, []
 
     def __repr__(self):
         # return str(self.rule_num)
         # print(self.rule_num)
+        return self.rule
         if isinstance(self.value, str):
             return self.value
 
@@ -99,27 +120,34 @@ class Rule:
 
 def day19p1():
     print('day 19 part 1')
-    # filename = 'day19_input.txt'
-    filename = 'day19_input_small.txt'
+    filename = 'day19_input.txt'
+    # filename = 'day19_input_small.txt'
     rules_dict, msgs = parse1(filename)
     print(rules_dict)
     base = Rule(rules_dict,0)
 
     # print(base.value)
 
-    print(base.eval('abbbbabbbbaaaababbbbbbaaaababb'))
+    # print(base.eval(['abbbbabbbbaaaababbbbbbaaaababb']))
     # babbbbaabbbbbabbbbbbaabaaabaaa
     #           bbbabbbbbbaabaaabaaa
-    # count = 0
-    # for m in msgs:
-    #     # print('** checking',m)
-    #     valid, leftover = base.eval(m)
-    #     if valid and len(leftover) == 0:
-    #         count += 1
-    #         print(m)
+    count = 0
+    for m in msgs:
+        # print('** checking',m)
+        valid, leftover = base.eval([m])
+        if valid and one_is_empty(leftover):
+            # print(leftover)
+            count += 1
+            # print(m)
 
-    # print(rules_dict, msgs)
-    # return count
+    print(rules_dict, msgs)
+    return count
+
+def one_is_empty(l):
+    for elem in l:
+        if len(elem) == 0:
+            return True
+    return False
 
 print(day19p1()) #1hr 20min
 
@@ -132,4 +160,4 @@ def day19p2():
     lines = ut.get_file('day19_input_small.txt', parse2)
     return
 
-print(day19p2())
+print(day19p2()) # 5hrs ffs
